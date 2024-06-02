a721c0f000a6266292334de176b3a86df6fce256
Rafael Dwiky <123283086+rafaeldwiky@users.noreply.github.com>
2024-06-02 15:30:36 +0700

    first push register

---
+++ app/src/main/java/com/dicoding/giziwase/bmi/bmiActivity.kt
@@ -0,0 +1,21 @@
+package com.dicoding.giziwase.bmi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.giziwase.R
+
+class bmiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bmi)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
