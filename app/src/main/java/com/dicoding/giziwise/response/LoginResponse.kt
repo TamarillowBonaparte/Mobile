7ab28ea372c77cf8b0577438b4f446e27ec3d762
MOH. DANI KURNIAWAN SUGIARTO <125776654+TamarillowBonaparte@users.noreply.github.com>
2024-06-11 16:18:25 +0700

    fix merah-merah dari signupsignin dan api

---
+++ app/src/main/java/com/dicoding/giziwise/response/LoginResponse.kt
@@ -0,0 +1,23 @@
+package com.dicoding.giziwise.response
+
+import com.google.gson.annotations.SerializedName
+data class LoginResponse(
+
+	@field:SerializedName("data")
+	val data: Data,
+
+	@field:SerializedName("message")
+	val message: String,
+
+	@field:SerializedName("status")
+	val status: String
+)
+
+data class Data(
+
+	@field:SerializedName("id")
+	val id: Int,
+
+	@field:SerializedName("token")
+	val token: String
+)
